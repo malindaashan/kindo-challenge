9ffe39c3de87f6657f90018b7a83daa9acb22956
Malinda <malinda.ashan@gmail.com>
2025-07-22 12:20:17 +1200

    deployment test 8

--- deployment.sh
+++ deployment.sh
@@ -17,13 +17,13 @@ npm run build
 
 sudo cp -r /opt/kindo-challenge/kindo-challenge-ui/build/* /opt/kindo-challenge/kindo-challenge-ui/static
 
-
+sudo chown -R ec2-user:ec2-user /opt/kindo-challenge/kindo-challenge-ui/static
 
 source /opt/kindo-challenge/kindochallengebackend/myenv/bin/activate
 
-cd  /opt/kindo-challenge/kindochallengebackend
+sudo cd  /opt/kindo-challenge/kindochallengebackend
 
-uvicorn /opt/kindo-challenge/kindochallengebackend/kindoapp.main:app --host 0.0.0.0 --port 8000 &
+uvicorn kindoapp.main:app --host 0.0.0.0 --port 8000 &
 
 sudo systemctl status nginx.service
 
